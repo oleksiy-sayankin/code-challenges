38093450545262ba4e52c63be907cdfe171056cb
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2015-07-27 15:07:54 +0300

    String Permutations. Challenge solved.

---
+++ string-permutations/src/main/java/net/javacogito/stringpermutations/Main.java
@@ -0,0 +1,61 @@
+package net.javacogito.stringpermutations;
+
+import java.io.*;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class Main {
+    private static TreeSet<String> result = new TreeSet<String>();
+
+    public static void main( String[] args ) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String inputLine;
+        while ((inputLine = buffer.readLine()) != null) {
+            inputLine = inputLine.trim();
+            permutations("", inputLine);
+            System.out.println(makeString(result));
+            result.clear();
+        }
+    }
+
+    public static void permutations(String first, String rest){
+        result.add(first + rest);
+        String nextRest = rest;
+        if (rest.length() == 1){
+            return;
+        }
+        do {
+            nextRest = rotate(nextRest);
+            permutations(first + getFirst(nextRest), getRest(nextRest));
+        }while (!rest.equals(nextRest));
+    }
+
+    private static String getFirst(String in){
+        return in.substring(0, 1);
+    }
+
+    private static String getRest(String in){
+        return in.substring(1);
+    }
+
+    private static  String rotate(String in){
+        return getRest(in) + getFirst(in);
+    }
+
+    private static String makeString(Set<String> lines){
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for(String line : lines){
+            if(first){
+                sb.append(line);
+                first = false;
+                continue;
+            }
+            sb.append(",");
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+}
---
+++ string-permutations/src/test/java/net/javacogito/stringpermutations/MainTest.java
@@ -0,0 +1,18 @@
+package net.javacogito.stringpermutations;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.io.IOException;
+import java.net.URL;
+
+
+public class MainTest {
+    @org.junit.Test
+    public void mainTest() throws IOException {
+        URL url = Thread.currentThread().getContextClassLoader().getResource("data001.in");
+        String[] input = {url.getPath()};
+        Main.main(input);
+    }
+}
