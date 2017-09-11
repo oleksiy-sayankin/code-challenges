93d07e0ec80e00ae0e263cc6536ab08c414500f7
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2017-09-11 10:21:11 +0300

    Color code converter. HSL to RGB color conversion

--- moderate/color-code-converter/src/main/java/net/javacogito/colorcodeconverter/Main.java
+++ moderate/color-code-converter/src/main/java/net/javacogito/colorcodeconverter/Main.java
@@ -24,17 +24,66 @@ public class Main {
 
   static String HslToRgb(int hue, int saturation, int lightness){
     double h = hue;
-    double s = saturation / 100;
-    double l = lightness / 100;
+    double s = (double) saturation / 100;
+    double l = (double) lightness / 100;
     double c = (1 - Math.abs(2 * l - 1)) * s;
     double x = c * (1 - Math.abs((h / 60)  % 2 - 1));
-    double m = l * c / 2;
-    double r_, g_, b_;
+    double m = l - c / 2;
+    double r_ = 0 , g_ = 0, b_ = 0;
+    double r, g, b;
 
+    if(0 <= h && h < 60){
+      r_ = c;
+      g_ = x;
+      b_ = 0;
+    }
 
+    if(60 <= h && h < 120){
+      r_ = x;
+      g_ = c;
+      b_ = 0;
+    }
+
+    if(120 <= h && h < 180){
+      r_ = 0;
+      g_ = c;
+      b_ = x;
+    }
+
+    if(180 <= h && h < 240){
+      r_ = 0;
+      g_ = x;
+      b_ = c;
+    }
+
+    if(240 <= h && h < 300){
+      r_ = x;
+      g_ = 0;
+      b_ = c;
+    }
+
+    if(300 <= h && h < 360){
+      r_ = c;
+      g_ = 0;
+      b_ = x;
+    }
+
+    r = (r_ + m) * 255;
+    g = (g_ + m) * 255;
+    b = (b_ + m) * 255;
+
+    int R = (int)Math.round(r);
+    int G = (int)Math.round(g);
+    int B = (int)Math.round(b);
 
     StringBuilder sb = new StringBuilder();
-
+    sb.append("RGB(");
+    sb.append(R);
+    sb.append(",");
+    sb.append(G);
+    sb.append(",");
+    sb.append(B);
+    sb.append(")");
 
     return sb.toString();
   }
--- moderate/color-code-converter/src/test/java/net/javacogito/colorcodeconverter/MainTest.java
+++ moderate/color-code-converter/src/test/java/net/javacogito/colorcodeconverter/MainTest.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.io.IOException;
 import java.net.URL;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
@@ -20,5 +21,29 @@ public class MainTest {
     long intervalMilliseconds = endTime - startTime;
     long intervalSeconds = intervalMilliseconds / 1000;
     System.out.println("duration = " + intervalSeconds);
+  }
+
+  @Test
+  public void HslToRgbTest(){
+    int hue1 = 359;
+    int saturation1 = 0;
+    int lightness1 = 0;
+
+    int hue2 = 117;
+    int saturation2 = 59;
+    int lightness2 = 79;
+
+    int hue3 = 0;
+    int saturation3 = 0;
+    int lightness3 = 50;
+
+
+    String RGB1 = "RGB(0,0,0)";
+    String RGB2 = "RGB(173,233,170)";
+    String RGB3 = "RGB(128,128,128)";
+
+    Assert.assertEquals(RGB1, Main.HslToRgb(hue1, saturation1, lightness1));
+    Assert.assertEquals(RGB2, Main.HslToRgb(hue2, saturation2, lightness2));
+    Assert.assertEquals(RGB3, Main.HslToRgb(hue3, saturation3, lightness3));
   }
 }
