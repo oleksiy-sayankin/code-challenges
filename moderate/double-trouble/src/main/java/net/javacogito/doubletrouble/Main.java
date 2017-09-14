e48b1d9f18cb356c205e6ffec626d2cb6103b2cb
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2017-09-14 14:31:18 +0300

    Double Trouble. Initial commit

---
+++ moderate/double-trouble/src/main/java/net/javacogito/doubletrouble/Main.java
@@ -0,0 +1,22 @@
+package net.javacogito.doubletrouble;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+  private static final String EMPTY = "";
+
+  public static void main(String[] args) throws IOException {
+    File file = new File(args[0]);
+    BufferedReader buffer = new BufferedReader(new FileReader(file));
+    String inputLine;
+    while ((inputLine = buffer.readLine()) != null) {
+      if (!EMPTY.equals(inputLine.trim())) {
+        System.out.println();
+      }
+    }
+  }
+}
