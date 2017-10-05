851acdfce18ccdbed539024ceca99151ddcebe8a
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2017-10-05 21:26:45 +0300

    Lowest Common Ancestor. Added tree

--- moderate/lowest-common-ancestor/src/main/java/net/javacogito/lowestcommonancestor/Main.java
+++ moderate/lowest-common-ancestor/src/main/java/net/javacogito/lowestcommonancestor/Main.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,6 +14,21 @@ import java.util.regex.Pattern;
 public class Main {
 
   private static final String EMPTY = "";
+  private static final Node tree = new Node("30");
+
+  static {
+    tree.setRoot(true);
+    Node node8 = new Node("8");
+    Node node52 = new Node("52");
+    tree.addChild(node8, node52);
+    Node node3 = new Node("2");
+    Node node20 = new Node("30");
+    node8.addChild(node3, node20);
+    Node node10 = new Node("10");
+    Node node29 = new Node("29");
+    node20.addChild(node10, node29);
+
+  }
 
   public static void main(String[] args) throws IOException {
     File file = new File(args[0]);
@@ -28,6 +44,21 @@ public class Main {
   private static class Node{
     private List<Node> children = new ArrayList<>();
     private boolean isRoot = false;
+    private final String name;
+
+    public Node getParent() {
+      return parent;
+    }
+
+    public void setParent(Node parent) {
+      this.parent = parent;
+    }
+
+    private Node parent;
+
+    public Node(String name){
+      this.name = name;
+    }
 
     private boolean isRoot(){
       return isRoot;
@@ -36,7 +67,28 @@ public class Main {
     private void setRoot(boolean isRoot){
       this.isRoot = isRoot;
     }
+    
+    private void addChild(Node ...nodes){
+      for(Node node : nodes) {
+        children.add(node);
+        node.setParent(this);
+      }
+    }
 
+    @Override
+    public String toString(){
+      return name;
+    }
 
+    private List<Node> pathToRoot(){
+      List<Node> result = new ArrayList<>();
+      Node currentNode = this;
+      while (!currentNode.isRoot()){
+        result.add(currentNode);
+        currentNode = currentNode.getParent();
+      }
+      Collections.reverse(result);
+      return result;
+    }
   }
 }
