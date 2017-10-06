382482fea414f3c76c09dff26c629b85018454ff
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2017-10-06 14:46:45 +0300

    Lowest Common Ancestor. Added partial solution

--- moderate/lowest-common-ancestor/src/main/java/net/javacogito/lowestcommonancestor/Main.java
+++ moderate/lowest-common-ancestor/src/main/java/net/javacogito/lowestcommonancestor/Main.java
@@ -5,29 +5,34 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
   private static final String EMPTY = "";
-  private static final Node tree = new Node("30");
+  static final Node ROOT = new Node("30");
+  private static final List<Node> ALL_NODES = new ArrayList<>();
 
   static {
-    tree.setRoot(true);
+    ROOT.setRoot(true);
     Node node8 = new Node("8");
     Node node52 = new Node("52");
-    tree.addChild(node8, node52);
-    Node node3 = new Node("2");
-    Node node20 = new Node("30");
+    ROOT.addChild(node8, node52);
+    Node node3 = new Node("3");
+    Node node20 = new Node("20");
     node8.addChild(node3, node20);
     Node node10 = new Node("10");
     Node node29 = new Node("29");
     node20.addChild(node10, node29);
 
+    ALL_NODES.add(ROOT);
+    ALL_NODES.add(node8);
+    ALL_NODES.add(node52);
+    ALL_NODES.add(node3);
+    ALL_NODES.add(node20);
+    ALL_NODES.add(node10);
+    ALL_NODES.add(node29);
   }
 
   public static void main(String[] args) throws IOException {
@@ -36,28 +41,66 @@ public class Main {
     String inputLine;
     while ((inputLine = buffer.readLine()) != null) {
       if (!EMPTY.equals(inputLine.trim())) {
-        System.out.println();
+        System.out.println(lowestCommonAncestor(parseNode1(inputLine), parseNode2(inputLine)));
       }
     }
   }
 
-  private static class Node{
+  private static Node parseNode1(String data){
+    return getNodeById(data.split(" ")[0]);
+  }
+
+  private static Node parseNode2(String data){
+    return getNodeById(data.split(" ")[1]);
+  }
+
+
+  static Node lowestCommonAncestor(Node node1, Node node2) {
+    List<Node> path1 = node1.pathToRoot();
+    List<Node> path2 = node2.pathToRoot();
+    int index = 0;
+    int length = Math.min(path1.size(), path2.size());
+    while (path1.get(index).equals(path2.get(index))) {
+      index++;
+      if(index >= length){
+        if(length == 1){
+          index = length;
+        } else {
+          index = index - 1;
+        }
+        break;
+      }
+    }
+    return path1.get(index - 1);
+  }
+
+
+  static Node getNodeById(String id){
+    for(Node node : ALL_NODES){
+      if(id.equals(node.getId())){
+        return node;
+      }
+    }
+    return ROOT; // never happens
+  }
+
+  static class Node{
     private List<Node> children = new ArrayList<>();
     private boolean isRoot = false;
-    private final String name;
+    private final String id;
 
-    public Node getParent() {
+    private Node getParent() {
       return parent;
     }
 
-    public void setParent(Node parent) {
+    private void setParent(Node parent) {
       this.parent = parent;
     }
 
     private Node parent;
 
-    public Node(String name){
-      this.name = name;
+    private Node(String id){
+      this.id = id;
     }
 
     private boolean isRoot(){
@@ -75,20 +118,38 @@ public class Main {
       }
     }
 
-    @Override
-    public String toString(){
-      return name;
+    private String getId(){
+      return id;
     }
 
-    private List<Node> pathToRoot(){
+    @Override
+    public String toString(){
+      return id;
+    }
+
+    List<Node> pathToRoot(){
       List<Node> result = new ArrayList<>();
       Node currentNode = this;
       while (!currentNode.isRoot()){
         result.add(currentNode);
         currentNode = currentNode.getParent();
       }
+      result.add(ROOT);
       Collections.reverse(result);
       return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Node node = (Node) o;
+      return id.equals(node.id);
+    }
+
+    @Override
+    public int hashCode() {
+      return id.hashCode();
     }
   }
 }
--- moderate/lowest-common-ancestor/src/test/java/net/javacogito/lowestcommonancestor/MainTest.java
+++ moderate/lowest-common-ancestor/src/test/java/net/javacogito/lowestcommonancestor/MainTest.java
@@ -5,7 +5,15 @@ import java.net.URL;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import  static net.javacogito.lowestcommonancestor.Main.Node;
+import  static net.javacogito.lowestcommonancestor.Main.ROOT;
+import static net.javacogito.lowestcommonancestor.Main.getNodeById;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
@@ -20,5 +28,43 @@ public class MainTest {
     long intervalMilliseconds = endTime - startTime;
     long intervalSeconds = intervalMilliseconds / 1000;
     System.out.println("duration = " + intervalSeconds);
+  }
+
+  @Test
+  public void getNodeByIdTest(){
+    Assert.assertEquals("20", Main.getNodeById("20").toString());
+    Assert.assertEquals("29", Main.getNodeById("29").toString());
+    Assert.assertEquals("3", Main.getNodeById("3").toString());
+  }
+
+
+  @Test
+  public void pathToRootTest(){
+    Node node20 = Main.getNodeById("20");
+    List<Node> pathToRoot20 = new ArrayList<>();
+    pathToRoot20.add(ROOT);
+    pathToRoot20.add(getNodeById("8"));
+    pathToRoot20.add(node20);
+    Assert.assertEquals(pathToRoot20, node20.pathToRoot());
+
+    Node node29 = Main.getNodeById("29");
+    List<Node> pathToRoot29 = new ArrayList<>();
+    pathToRoot29.add(ROOT);
+    pathToRoot29.add(getNodeById("8"));
+    pathToRoot29.add(getNodeById("20"));
+    pathToRoot29.add(node29);
+    Assert.assertEquals(pathToRoot29, node29.pathToRoot());
+
+  }
+
+  @Test
+  public void lowestCommonAncestorTest(){
+//    Assert.assertEquals("30", Main.lowestCommonAncestor(Main.getNodeById("8"), Main.getNodeById("20")).toString());
+//    Assert.assertEquals("30", Main.lowestCommonAncestor(Main.getNodeById("8"), Main.getNodeById("52")).toString());
+//    Assert.assertEquals("8", Main.lowestCommonAncestor(Main.getNodeById("3"), Main.getNodeById("29")).toString());
+//    Assert.assertEquals("30", Main.lowestCommonAncestor(Main.getNodeById("29"), Main.getNodeById("8")).toString());
+//    Assert.assertEquals("20", Main.lowestCommonAncestor(Main.getNodeById("10"), Main.getNodeById("29")).toString());
+    Assert.assertEquals("30", Main.lowestCommonAncestor(Main.getNodeById("8"), Main.getNodeById("30")).toString());
+    System.out.println();
   }
 }
