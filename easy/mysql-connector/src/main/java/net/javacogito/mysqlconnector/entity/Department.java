ec86144c91818cda7960a55d3210e6516929292f
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2018-10-10 13:10:46 +0300

    Add comments to Department

--- easy/mysql-connector/src/main/java/net/javacogito/mysqlconnector/entity/Department.java
+++ easy/mysql-connector/src/main/java/net/javacogito/mysqlconnector/entity/Department.java
@@ -10,18 +10,34 @@ public final class Department {
   private int id;
   private String name;
 
+  /**
+   * Gets id.
+   * @return id
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * Sets id.
+   * @param id primary key
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * Gets name.
+   * @return name
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Sets name.
+   * @param name department name.
+   */
   public void setName(String name) {
     this.name = name;
   }
