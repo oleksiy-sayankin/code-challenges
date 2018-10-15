5c3d7ea378a9c5e4b43f3df3536c5efc89b5b084
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2018-10-15 17:13:52 +0300

    Added data loading methods and sample data

--- easy/mysql-connector/src/main/java/net/javacogito/mysqlconnector/Main.java
+++ easy/mysql-connector/src/main/java/net/javacogito/mysqlconnector/Main.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 
 import static net.javacogito.mysqlconnector.util.DbUtil.initDb;
 import static net.javacogito.mysqlconnector.util.DbUtil.storeInDb;
-import static net.javacogito.mysqlconnector.util.LoadTableUtil.loadCountries;
 import static net.javacogito.mysqlconnector.util.FileUtil.getFromResources;
-import static net.javacogito.mysqlconnector.util.LoadTableUtil.loadCustomers;
+import static net.javacogito.mysqlconnector.util.LoadTableUtil.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
@@ -14,7 +13,7 @@ public class Main {
   }
 
   /**
-   * This method initializes DB, loads data from CSV file and stores it in DB.
+   * This method initializes DB, loads data from CSV file and stores it in DB with JDBC connector.
    *
    * @return true if creation completes successfully
    * @throws IOException when can't read data from file
@@ -24,6 +23,10 @@ public class Main {
     initDb();
     storeInDb(loadCountries(getFromResources("country.csv")));
     storeInDb(loadCustomers(getFromResources("customer.csv")));
+    storeInDb(loadDepartments(getFromResources("department.csv")));
+    storeInDb(loadEmployees(getFromResources("employee.csv")));
+    storeInDb(loadEmployeeEmails(getFromResources("employee-email.csv")));
+    storeInDb(loadEmployeePhones(getFromResources("employee-phone.csv")));
     return true;
   }
 }
--- easy/mysql-connector/src/main/java/net/javacogito/mysqlconnector/util/LoadTableUtil.java
+++ easy/mysql-connector/src/main/java/net/javacogito/mysqlconnector/util/LoadTableUtil.java
@@ -3,14 +3,13 @@ package net.javacogito.mysqlconnector.util;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import net.javacogito.mysqlconnector.entity.Country;
-import net.javacogito.mysqlconnector.entity.Customer;
+
+import net.javacogito.mysqlconnector.entity.*;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
-import static net.javacogito.mysqlconnector.util.EntityUtil.createCountry;
-import static net.javacogito.mysqlconnector.util.EntityUtil.createCustomer;
+import static net.javacogito.mysqlconnector.util.EntityUtil.*;
 
 /**
  * Util class for loading data from CSV file and representing it as a List of entities.
@@ -58,5 +57,87 @@ public final class LoadTableUtil {
     return customers;
   }
 
+  /**
+   * Loads departments from CSV file into list of entities.
+   *
+   * @param departmentsFile departments File
+   * @return list of entities
+   * @throws IOException when can't read data
+   */
+  public static List<Department> loadDepartments(File departmentsFile) throws IOException {
+    List<Department> departments = new ArrayList<>();
+    try(
+        Reader reader = new FileReader(departmentsFile);
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+    ) {
+      for (CSVRecord csvRecord : csvParser) {
+        departments.add(createDepartment(Integer.parseInt(csvRecord.get(0)), csvRecord.get(1)));
+      }
+    }
+    return departments;
+  }
+
+
+  /**
+   * Loads employees from CSV file into list of entities.
+   *
+   * @param employeesFile employees File
+   * @return list of entities
+   * @throws IOException when can't read data
+   */
+  public static List<Employee> loadEmployees(File employeesFile) throws IOException {
+    List<Employee> employees = new ArrayList<>();
+    try(
+        Reader reader = new FileReader(employeesFile);
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+    ) {
+      for (CSVRecord csvRecord : csvParser) {
+        employees.add(createEmployee(Integer.parseInt(csvRecord.get(0)), csvRecord.get(1), csvRecord.get(2), Integer.parseInt(csvRecord.get(3)),
+            Integer.parseInt(csvRecord.get(4)), Integer.parseInt(csvRecord.get(5)), Float.parseFloat(csvRecord.get(6))));
+      }
+    }
+    return employees;
+  }
+
+  /**
+   * Loads employee emails from CSV file into list of entities.
+   *
+   * @param employeeEmailsFile employee emails File
+   * @return list of entities
+   * @throws IOException when can't read data
+   */
+  public static List<EmployeeEmail> loadEmployeeEmails(File employeeEmailsFile) throws IOException {
+    List<EmployeeEmail> employees = new ArrayList<>();
+    try(
+        Reader reader = new FileReader(employeeEmailsFile);
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+    ) {
+      for (CSVRecord csvRecord : csvParser) {
+        employees.add(createEmployeeEmail(Integer.parseInt(csvRecord.get(0)), Integer.parseInt(csvRecord.get(1)), csvRecord.get(2)));
+      }
+    }
+    return employees;
+  }
+
+
+  /**
+   * Loads employee phones from CSV file into list of entities.
+   *
+   * @param employeePhonesFile employee phones File
+   * @return list of entities
+   * @throws IOException when can't read data
+   */
+  public static List<EmployeePhone> loadEmployeePhones(File employeePhonesFile) throws IOException {
+    List<EmployeePhone> employeePhones = new ArrayList<>();
+    try(
+        Reader reader = new FileReader(employeePhonesFile);
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+    ) {
+      for (CSVRecord csvRecord : csvParser) {
+        employeePhones.add(createEmployeePhone(Integer.parseInt(csvRecord.get(0)), Integer.parseInt(csvRecord.get(1)), csvRecord.get(2)));
+      }
+    }
+    return employeePhones;
+  }
 
 }
