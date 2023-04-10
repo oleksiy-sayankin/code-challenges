efc7992c421e386a0bfd5265b4934da443777444
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2023-04-10 22:52:05 +0300

    Added two solutions for merge sorted arrays challange

--- algorithms/easy/mergesortedarrays/src/main/java/net/javacogito/mergesortedarrays/Kata.java
+++ algorithms/easy/mergesortedarrays/src/main/java/net/javacogito/mergesortedarrays/Kata.java
@@ -1,15 +1,50 @@
 package net.javacogito.mergesortedarrays;
 
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Kata {
 
-  public static int[] mergeSorted(int[]... arrays) {
+  public static int[] mergeSortedWithLinkedList(int[]... arrays) {
+    List<Integer> integers = new LinkedList<>();
+    for (int[] array : arrays) {
+      for (int value : array) {
+        integers.add(value);
+      }
+    }
+    integers.sort(Comparator.comparingInt(o -> o));
+    int size = integers.size();
+    int[] result = new int[size];
+    int i = 0;
+    for (Integer integer : integers) {
+      result[i++] = integer;
+    }
+    return result;
+  }
+
+
+  public static int[] mergeSortedWithTreeSet(int[]... arrays) {
+    SortedSet<Integer> integers = new TreeSet<>();
+    for (int[] array : arrays) {
+      for (int value : array) {
+        integers.add(value);
+      }
+    }
+    int size = integers.size();
+    int[] result = new int[size];
+    int i = 0;
+    for (Integer integer : integers) {
+      result[i++] = integer;
+    }
+    return result;
+  }
+
+
+  public static int[] mergeSortedWithPriorityQueue(int[]... arrays) {
     PriorityQueue<Integer> queue = new PriorityQueue<>();
     int length = 0;
-    for(int[] array : arrays){
+    for (int[] array : arrays) {
       length += array.length;
-      for(int value : array){
+      for (int value : array) {
         queue.add(value);
       }
     }
--- algorithms/easy/mergesortedarrays/src/test/java/net/javacogito/mergesortedarrays/KataTest.java
+++ algorithms/easy/mergesortedarrays/src/test/java/net/javacogito/mergesortedarrays/KataTest.java
@@ -2,15 +2,31 @@ package net.javacogito.mergesortedarrays;
 
 import org.junit.Test;
 
-import static net.javacogito.mergesortedarrays.Kata.mergeSorted;
+import static net.javacogito.mergesortedarrays.Kata.*;
 import static org.junit.Assert.assertArrayEquals;
 
 public class KataTest {
 
   @Test
-  public void testMergeSorted() {
-    assertArrayEquals(new int[]{1, 3, 5, 7, 9, 11, 13, 14, 16, 18, 19, 20, 21, 22}, mergeSorted(new int[]{1, 3, 5, 7, 9, 11, 13}, new int[]{14, 16, 18}, new int[]{19, 20, 21, 22}));
-    assertArrayEquals(new int[]{1, 5, 7, 10, 15, 17, 20, 25, 27, 30, 35, 37}, mergeSorted(new int[]{1, 10, 20, 30}, new int[]{5, 15, 25, 35}, new int[]{7, 17, 27, 37}));
-    assertArrayEquals(new int[]{3, 5, 7, 33, 55, 77, 333, 555, 777}, mergeSorted(new int[]{777, 77, 7}, new int[]{555, 55, 5}, new int[]{333, 33, 3}));
+  public void testMergeSortedWithPriorityQueue() {
+    assertArrayEquals(new int[]{1, 3, 5, 7, 9, 11, 13, 14, 16, 18, 19, 20, 21, 22}, mergeSortedWithPriorityQueue(new int[]{1, 3, 5, 7, 9, 11, 13}, new int[]{14, 16, 18}, new int[]{19, 20, 21, 22}));
+    assertArrayEquals(new int[]{1, 5, 7, 10, 15, 17, 20, 25, 27, 30, 35, 37}, mergeSortedWithPriorityQueue(new int[]{1, 10, 20, 30}, new int[]{5, 15, 25, 35}, new int[]{7, 17, 27, 37}));
+    assertArrayEquals(new int[]{3, 5, 7, 33, 55, 77, 333, 555, 777}, mergeSortedWithPriorityQueue(new int[]{777, 77, 7}, new int[]{555, 55, 5}, new int[]{333, 33, 3}));
   }
+
+  @Test
+  public void testMergeSortedWithSortedSet() {
+    assertArrayEquals(new int[]{1, 3, 5, 7, 9, 11, 13, 14, 16, 18, 19, 20, 21, 22}, mergeSortedWithTreeSet(new int[]{1, 3, 5, 7, 9, 11, 13}, new int[]{14, 16, 18}, new int[]{19, 20, 21, 22}));
+    assertArrayEquals(new int[]{1, 5, 7, 10, 15, 17, 20, 25, 27, 30, 35, 37}, mergeSortedWithTreeSet(new int[]{1, 10, 20, 30}, new int[]{5, 15, 25, 35}, new int[]{7, 17, 27, 37}));
+    assertArrayEquals(new int[]{3, 5, 7, 33, 55, 77, 333, 555, 777}, mergeSortedWithTreeSet(new int[]{777, 77, 7}, new int[]{555, 55, 5}, new int[]{333, 33, 3}));
+  }
+
+  @Test
+  public void testMergeSortedWithLinkedList() {
+    assertArrayEquals(new int[]{1, 3, 5, 7, 9, 11, 13, 14, 16, 18, 19, 20, 21, 22}, mergeSortedWithLinkedList(new int[]{1, 3, 5, 7, 9, 11, 13}, new int[]{14, 16, 18}, new int[]{19, 20, 21, 22}));
+    assertArrayEquals(new int[]{1, 5, 7, 10, 15, 17, 20, 25, 27, 30, 35, 37}, mergeSortedWithLinkedList(new int[]{1, 10, 20, 30}, new int[]{5, 15, 25, 35}, new int[]{7, 17, 27, 37}));
+    assertArrayEquals(new int[]{3, 5, 7, 33, 55, 77, 333, 555, 777}, mergeSortedWithLinkedList(new int[]{777, 77, 7}, new int[]{555, 55, 5}, new int[]{333, 33, 3}));
+  }
+
+
 }
