87c72eeffdcd18138f40d518d7efd36c080a6158
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2023-04-21 22:55:14 +0300

    Part I

---
+++ algorithms/easy/countthesmileyfaces/src/main/java/net/javacogito/countthesmileyfaces/SmileFaces.java
@@ -0,0 +1,10 @@
+package net.javacogito.countthesmileyfaces;
+
+import java.util.List;
+
+public class SmileFaces {
+  public static int countSmileys(List<String> arr) {
+    // Just Smile :)
+    return 0;
+  }
+}
